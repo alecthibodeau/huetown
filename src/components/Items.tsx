c9dc179f7097d481acb6b7f6d6b3ad57ae9c293f
alec t <alec@alect.me>
2023-03-11 06:31:38 -0500

    Update Items component

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -8,27 +8,32 @@ import ItemProps from '../interfaces/ItemProps';
 
 function Items() {
 
-  function renderItemPreview(props: ItemProps, index: number) {
+  function getItemPreviewCategory(item: ItemProps): string {
+    return item.itemCategory === itemsConstants.textStrings.categoryLunarCalendar
+    ? `${item.itemLunarCalendarYear} ${itemsConstants.textStrings.categoryLunarCalendar} ${itemsConstants.textStrings.categoryPrint}`
+    : item.itemCategory
+  }
 
+  function renderItemPreview(item: ItemProps, index: number) {
     return (
-      <Link key={props.itemTitle + index} className="item-preview text-link" to="/">
-        <img className="item-preview-image" src={props.itemImageFront} />
+      <Link key={item.itemTitle + index} className="item-preview text-link" to="/">
+        <img
+          className="item-preview-image"
+          src={item.itemImageFront}
+          alt={`${item.itemTitle} preview`}
+        />
         <div className="item-preview-info">
           <div className="item-preview-title">
-            {props.itemTitle}
+            {item.itemTitle}
           </div>
           <div className="item-preview-category">
-            {
-              props.itemCategory === itemsConstants.textStrings.categoryLunarCalendar
-                ? `${props.itemLunarCalendarYear} ${itemsConstants.textStrings.categoryLunarCalendar} ${itemsConstants.textStrings.categoryPrint}`
-                : props.itemCategory
-            }
+            {getItemPreviewCategory(item)}
           </div>
           {/* <div className="item-preview-description">
-            {props.itemInfo[1].toLowerCase()}
+            {item.itemInfo[1].toLowerCase()}
           </div> */}
           <div className="item-preview-price">
-            {`$${props.itemPrice}`}
+            {`$${item.itemPrice}`}
           </div>
         </div>
       </Link>
