e95448aa265298adf133600c883c060dae652bcb
alec t <alec@alect.me>
2023-05-19 19:00:00 -0400

    Update annual data

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -45,14 +45,14 @@ function Home(props: { date: Date }): JSX.Element {
         />
       :
         <div className="home-content">
-          <div className="home-image-container">
+          {/* <div className="home-image-container">
             <a href="https://www.instagram.com/p/Cq-889Wrfbt">
               <img src={images.framedPiece} alt="framed ink drawing by Alec Thibodeau"/>
             </a>
           </div>
           <div className="home-text-container">
             <span className="bold">Recently:</span> <span>Alec donated this <a className="text-link" href="https://www.instagram.com/p/Cq-889Wrfbt">original ink drawing</a> to <a className="text-link" href="https://newurbanarts.org/about">New Urban Arts</a> for the organization's 26th Annual Birthday Bash and Silent Art Auction.</span>
-          </div>
+          </div> */}
           <div>
             <div className="date-container">
               <div className="phase-container">
