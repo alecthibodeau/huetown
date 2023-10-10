f8355179a6e799f87c0237b292231da7af96682d
alec t <alec@alect.me>
2023-10-09 23:57:20 -0400

    Add file for 2022 lunar phases

---
+++ src/constants/lunar-phases-2022.ts
@@ -0,0 +1,14 @@
+/* Interfaces */
+import LunarPhaseDates from '../interfaces/LunarPhaseDates';
+
+/* Helpers */
+import digitalLunarCalendar from '../helpers/digital-lunar-calendar';
+
+const {} = digitalLunarCalendar.lunarPhases;
+
+const lunarPhaseDates2022: LunarPhaseDates = {
+  // Previous year, 2021: third quarter moon was on 12/26/2021
+  // Following year, 2023: full moon will be on 1/6/2023
+};
+
+export default lunarPhaseDates2022;
