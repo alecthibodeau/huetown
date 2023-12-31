7003485df415a6e255f3f9478d475113202e9ab4
alec t <alec@alect.me>
2023-12-31 17:41:26 -0500

    Assign object to variable before exporting

--- src/constants/items-specific-info.ts
+++ src/constants/items-specific-info.ts
@@ -4,7 +4,9 @@ import items from './items';
 const latestLunarCalendarId: string = items.lunarCalendar2024.id;
 const preorderId: string = 'preorder id goes here';
 
-export default {
+const itemsSpecificInfo = {
   latestLunarCalendarId,
   preorderId
 };
+
+export default itemsSpecificInfo;
