11eeccc077d9f23d6a0ab624ed3ae045be0d5309
alec t <alec@alect.me>
2024-01-01 11:13:14 -0500

    Adjust color variables

--- src/constants/lunar-calendars-information.ts
+++ src/constants/lunar-calendars-information.ts
@@ -8,14 +8,17 @@ import lunarPhaseDates2024 from './lunar-phases-2024';
 /* Helpers */
 import setIntermediateLunarPhasesDates from '../helpers/set-intermediate-lunar-phases-dates';
 
+const twentyThreeBluePaper: string = '#008eb2';
+const twentyFourPlumPaper: string = '#a3469c';
+
 const lunarCalendarsInformation: LunarCalendarsInformation = {
   2023: {
-    backgroundColor: '#008eb2',
+    backgroundColor: twentyThreeBluePaper,
     ornaments: ['apey', 'horsey', 'doggy'],
     lunarPhaseDates: setIntermediateLunarPhasesDates(lunarPhaseDates2023, 2023)
   },
   2024: {
-    backgroundColor: '#a6439e',
+    backgroundColor: twentyFourPlumPaper,
     ornaments: ['bunny', 'birdy', 'skunky'],
     lunarPhaseDates: setIntermediateLunarPhasesDates(lunarPhaseDates2024, 2024)
   }
