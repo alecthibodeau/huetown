65c8e18ea5b80c4c09105ccae13256635a1cb0dd
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2024-12-07 10:41:37 -0500

    refactor: update ItemLunarCalendarInfo component to improve readability

--- src/components/ItemLunarCalendarInfo.tsx
+++ src/components/ItemLunarCalendarInfo.tsx
@@ -16,8 +16,11 @@ import itemsLunarCalendars from '../constants/items-lunar-calendars';
 import itemsSpecificInfo from '../constants/items-specific-info';
 
 function ItemLunarCalendarInfo(props: ItemProps): JSX.Element {
+  const { title, lunarCalendarYear, printEdition, lunarCalendarPaper, lunarCalendarLocation } = props;
+  const { latestLunarCalendarId, preorderId } = itemsSpecificInfo;
   const isNewsInInfo: boolean = false;
-  const isPreorder: boolean = props.id === itemsSpecificInfo.preorderId;
+  const isPreorder: boolean = props.id === preorderId;
+  const isLatestLunarCalendar: boolean = props.id === latestLunarCalendarId;
 
   function renderlunarPhase(phase: LunarPhase, index: number): JSX.Element {
     const moonTextFormatted: string = `${phase.name} moon`;
@@ -74,62 +77,48 @@ function ItemLunarCalendarInfo(props: ItemProps): JSX.Element {
     )
   }
 
+  function renderSpan(content: string | number | undefined, isTitle?: boolean): JSX.Element {
+    return (
+      <span className={isTitle ? 'ital' : undefined}>{content}</span>
+    );
+  }
+
   return (
     <>
       <div className="lunar-calendar-description">
-
-      {isPreorder ?
-          <div>
-            <p>
-              <span className="bold">Preorder 2025 Lunar Calendar</span>: Okay,
-              my lunar calendar for 2025 is currently in production at the print
-              shop &mdash; with Dan making the letterpress magic happen for another
-              edition. Preorders placed after November 19th, 2024 will ship the first
-              week of December 2024. Huetown's domestic shipping charge via USPS
-              Priority (or equivalent) is a flat, per-order rate of $8 rate to
-              any address in the USA.
-            </p>
-            <p>
-              <span className="bold">Important</span>: The above lunar calendar
-              image is a digital-color simulation of what the final piece's inks
-              and paper will look like. There may be some slight variations
-              between the colors here and in the print.
-            </p>
-            <p>
-              <span className="bold">Shipping</span>: The 2025 lunar calendar <span className="ital">ships
-              flat</span> in one sturdy box, regardless of quantity in the order.
-            </p>
-            <p id="itemDetails">
-              <span className="bold">Updates</span>: Follow <a className="text-link"
-              href="https://www.instagram.com/huetown/">@huetown</a> on
-              Instagram for the latest word on this print's letterpress production.
-              And join the <Link className="text-link" to="/about">email
-              list</Link> for more news.
-            </p>
-            <p className="bold">
-              Here's more info on the ultimate piece&hellip;
-            </p>
-          </div>
-        : null}
-
-        <div className="lunar-calendar-paragraph">
-          <span className="lunar-calendar-title ital">{props.title}</span> is a letterpress-printed lunar calendar designed and drawn by Alec Thibodeau (me!),
-          chronicling all phases of the moon for <span className="lunar-calendar-year">{props.lunarCalendarYear}</span>.
-          From an edition of <span className="print-edition">{props.printEdition}</span> prints <span className="lunar-calendar-title ital">{props.title}</span> continues Alec's ongoing lunar calendar series,
-          which features ornate illustrations framing an annual cycle of moon shapes.
-          As with each of Alec's previous lunar calendars this piece measures 19" x 11.75" (a golden rectangle) and is expertly printed by Dan Wood
-          and his team at <a className="text-link" href="https://www.dwriletterpress.net">DWRI&nbsp;Letterpress</a>.
-        </div>
-        <div className="lunar-calendar-paragraph">
-          The <span className="lunar-calendar-year">{props.lunarCalendarYear}</span> lunar calendar comprises new drawings and year-specific lunar phase information.
-          Each calendar is printed on <span className="lunar-calendar-paper-info">{props.lunarCalendarPaper}</span>, blind-debossed with Alec's name and the print shop's name &mdash; and hand numbered and signed by Alec. Each calendar ships with an accompanying information sheet inside a flat, sturdy box.
-        </div>
-        <div className="lunar-calendar-paragraph">
-          The calendar's lunar phase chart is calibrated for any location in the Eastern Time Zone and is accurate to within a day for anywhere in the Northern Hemisphere. The chart also matches lunar cycles in the Southern Hemisphere: but with inverted shapes. <span className="lunar-calendar-location">{props.lunarCalendarLocation}</span>
-        </div>
-        <div className="lunar-calendar-paragraph">
-          Custom moon illustrations portray all principal lunar phases (New Moon, First Quarter Moon, Full Moon and Third Quarter Moon) plus all intermediate crescent and gibbous phases. For months with less than 31 days, drawings of clouds occupy the extra spaces. <a className="text-link" href="#featureImage">Order now</a>.
-        </div>
+        <p>
+          {renderSpan(title, true)} is a letterpress-printed lunar calendar
+          designed and drawn by Alec Thibodeau (me!), chronicling all phases of
+          the moon for {renderSpan(lunarCalendarYear)}. From an edition
+          of {renderSpan(printEdition)} prints {renderSpan(title, true)} continues
+          Alec's ongoing lunar calendar series, which features ornate illustrations
+          framing an annual cycle of moon shapes. As with each of Alec's previous
+          lunar calendars this piece measures 19" x 11.75" (a golden rectangle)
+          and is expertly printed by Dan Wood and his team
+          at <a className="text-link" href="https://www.dwriletterpress.net">
+          DWRI&nbsp;Letterpress</a>.
+        </p>
+        <p>
+          The {renderSpan(lunarCalendarYear)} lunar calendar comprises new drawings
+          and year-specific lunar phase information. Each calendar is printed
+          on {renderSpan(lunarCalendarPaper)}, blind-debossed with Alec's name
+          and the print shop's name &mdash; and hand numbered and signed by
+          Alec. Each calendar ships with an accompanying information sheet inside
+          a flat, sturdy box.
+        </p>
+        <p>
+          The calendar's lunar phase chart is calibrated for any location in the
+          Eastern Time Zone and is accurate to within a day for anywhere in the
+          Northern Hemisphere. The chart also matches lunar cycles in the Southern
+          Hemisphere: but with inverted shapes. {renderSpan(lunarCalendarLocation)}
+        </p>
+        <p>
+          Custom moon illustrations portray all principal lunar phases (New Moon,
+          First Quarter Moon, Full Moon and Third Quarter Moon) plus all
+          intermediate crescent and gibbous phases. For months with less than
+          31 days, drawings of clouds occupy the extra
+          spaces. <a className="text-link" href="#featureImage">Order now</a>.
+        </p>
         <div className="lunar-phases">
           {itemsLunarCalendars.phases.map(renderlunarPhase)}
         </div>
@@ -144,16 +133,15 @@ function ItemLunarCalendarInfo(props: ItemProps): JSX.Element {
             accompanying <Link className="text-link" to="/moon-space">digital
             lunar calendar</Link>
           </li>
-          {props.id === itemsSpecificInfo.latestLunarCalendarId ?
+          {isLatestLunarCalendar ?
             <li>
               <span className="bold">Extra</span>: Each lunar calendar preordered
-              by November 19th, 2024 included a <span className="ital">Tarsier
-              Trail</span> letterpress print&hellip;
+              early included a <span className="ital">Tarsier Trail</span> letterpress
+              print&hellip;
             </li>
           : null}
         </ul>
-
-        {props.id === itemsSpecificInfo.latestLunarCalendarId ?
+        {isLatestLunarCalendar ?
           <div className="lunar-calendar-accompanying-item">
             <img src={images.lunarCalendars.tarsierTrailPreorder} alt="Tarsier Trail print" />
           </div>
