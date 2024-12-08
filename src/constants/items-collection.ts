293185a67a5eee19fd87286293270b1d84dabb62
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2024-12-08 09:07:08 -0500

    feat: rearrange order of shop items to improve variety

--- src/constants/items-collection.ts
+++ src/constants/items-collection.ts
@@ -6,16 +6,7 @@ import items from './items';
 
 const itemsCollection: ItemProps[] = [
   items.lunarCalendar2025,
-  items.lunarCalendar2024,
-  items.lunarCalendar2023,
-  items.lunarCalendar2022,
-  items.lunarCalendar2021,
-  items.lunarCalendar2020,
-  items.lunarCalendar2019,
-  items.lunarCalendar2018,
-  items.lunarCalendar2017,
   items.printAbundance,
-  items.printFunktionslust,
   items.printOrcaLumina,
   items.printPanTerra,
   items.printProtonSwan,
@@ -26,7 +17,15 @@ const itemsCollection: ItemProps[] = [
   items.postcardCachalot,
   items.postcardGreatWhiteShark,
   items.postcardHippopotamus,
-  items.postcardRhodeIslandWhale
+  items.postcardRhodeIslandWhale,
+  items.lunarCalendar2024,
+  items.lunarCalendar2023,
+  items.lunarCalendar2022,
+  items.lunarCalendar2021,
+  items.lunarCalendar2020,
+  items.lunarCalendar2019,
+  items.lunarCalendar2018,
+  items.lunarCalendar2017
 ];
 
 export default itemsCollection;
