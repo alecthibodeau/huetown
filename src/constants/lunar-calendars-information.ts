3238610aeae15f0786a2790e6b8b36f531f23ce5
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2024-12-31 20:05:34 -0500

    feat: add info for 2025 lunar calendar

--- src/constants/lunar-calendars-information.ts
+++ src/constants/lunar-calendars-information.ts
@@ -4,12 +4,14 @@ import LunarCalendarsInformation from '../interfaces/LunarCalendarsInformation';
 /* Constants */
 import lunarPhaseDates2023 from './lunar-phases-2023';
 import lunarPhaseDates2024 from './lunar-phases-2024';
+import lunarPhaseDates2025 from './lunar-phases-2025';
 
 /* Helpers */
 import setIntermediateLunarPhasesDates from '../helpers/set-intermediate-lunar-phases-dates';
 
 const twentyThreeBluePaper: string = '#008eb2';
 const twentyFourPlumPaper: string = '#a3469c';
+const twentyFiveKellyGreenPaper: string = '#178d6a';
 
 const lunarCalendarsInformation: LunarCalendarsInformation = {
   2023: {
@@ -21,6 +23,11 @@ const lunarCalendarsInformation: LunarCalendarsInformation = {
     backgroundColor: twentyFourPlumPaper,
     ornaments: ['bunny', 'birdy', 'skunky'],
     lunarPhaseDates: setIntermediateLunarPhasesDates(lunarPhaseDates2024, 2024)
+  },
+  2025: {
+    backgroundColor: twentyFiveKellyGreenPaper,
+    ornaments: ['kitty', 'beasty', 'loony'],
+    lunarPhaseDates: setIntermediateLunarPhasesDates(lunarPhaseDates2025, 2025)
   }
 };
 
