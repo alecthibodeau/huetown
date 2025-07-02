08a3e67007df4bb2a83839ff84a930893c259944
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2025-07-02 08:35:06 -0400

    feat: set boolean variable to extend shop active state

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ import formatText from './helpers/format-text';
 function App(): JSX.Element {
   const [isBreakpointXs, setIsBreakpointXs] = useState<boolean>(true);
   const [viewportWidth, setViewportWidth] = useState<number>(window.innerWidth);
-  const isShopActive: boolean = new Date() < new Date('2025-07-01');
+  const isShopActive: boolean = new Date() < new Date('2026-01-01');
 
   useEffect(() => {
     const breakpointSm: number = 576;
