15685e2c562e3b2b53125934a83aa9a9093ba3e7
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2025-08-13 11:53:45 -0400

    feat: update shops array to include additional item

--- src/constants/shops.ts
+++ src/constants/shops.ts
@@ -15,6 +15,11 @@ const shops: Shop[] = [
     location: 'Providence (East Side)',
     name: 'Like No Udder',
     url: 'https://www.like-no-udder.com'
+  },
+  {
+    location: 'Wakefield',
+    name: 'Eclectic Energy',
+    url: 'https://www.eclecticenergy.com'
   }
 ];
 
