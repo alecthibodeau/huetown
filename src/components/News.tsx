7f63519f8971458114b91d49887cce8d396e1682
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2025-09-19 15:35:34 -0400

    style: adjust news component elements to improve layout

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -6,7 +6,7 @@ function News(): JSX.Element {
     <div className="news">
       <div className="news-text-wrapper">
         <span className="bold">Event</span>: Join Huetown
-        November 22nd and 23rd at the <a className="text-link"
+        November 22nd &amp; 23rd at the <a className="text-link"
         href="https://goodtrademakersmarket.com" target="_blank">2025
         Good Trade Makers Market</a> in Providence, Rhode Island.
       </div>
