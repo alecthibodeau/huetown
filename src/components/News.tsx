ea26bf2a70ab3749a22809e4bbdb8fb03ba3e0d0
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2026-02-05 09:48:58 -0500

    feat: update news component text to clarify upcoming event information

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 /* Styles */
 import '../styles/news.scss';
 
@@ -6,9 +8,11 @@ function News(): JSX.Element {
     <div className="news">
       <div className="news-text-wrapper">
         <span className="bold">Event</span>: Join Huetown
-        February 21st &amp; 22nd at the <a className="text-link"
+        at the <a className="text-link"
         href="https://www.rivegfest.com" target="_blank">2026
-        Rhode Island VegFest</a> in Providence, Rhode Island.
+        Rhode Island VegFest</a> on February 21st &amp; 22nd. Yes, prints of
+        the <Link className="text-link" to="/shop/lunar-calendars/sigil-routing">2026 lunar
+        calendar</Link> will be available there.
       </div>
     </div>
   );
