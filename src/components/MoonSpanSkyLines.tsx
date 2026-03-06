a04bdcf40f6f1964d5eefe6976e322c3162f81b2
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2026-03-06 10:35:40 -0500

    feat: use effect hook to add or remove sky segments

--- src/components/MoonSpanSkyLines.tsx
+++ src/components/MoonSpanSkyLines.tsx
@@ -1,22 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const MoonSpanSkyLines = React.memo(() => {
-  const breakpointXl: number = 1400;
-  const maxSkyLineWidth: number = Math.min(window.innerWidth, breakpointXl);
-  const skySegmentsCount: number = Math.floor(maxSkyLineWidth/2 - 8);
+  const textMoonSpanSkyLine: string = 'moon-span-sky-line';
   const textMoonSpanSkySegment: string = 'moon-span-sky-segment';
-  const textSkyStar: string = 'sky-star';
 
-  function renderSkySegment(skyLineIndex: number, skySegmentIndex: number): JSX.Element {
+  useEffect(() => {
+    const textResize: string = 'resize';
+    const handleResize = () => {
+      const rows: NodeListOf<Element> = document.querySelectorAll(`.${textMoonSpanSkyLine}`);
+      const currentSkySegmentCount: number = rows[0].childElementCount;
+      const segmentsDifference: number = getSkySegmentCount() - currentSkySegmentCount;
+      if (segmentsDifference > 0) {
+        rows.forEach(row => {
+          for (let i = 0; i < segmentsDifference; i++) {
+            const skySegment: HTMLDivElement = document.createElement('div');
+            skySegment.className = getSkySegmentClass();
+            row.appendChild(skySegment);
+          }
+        });
+      } else {
+        rows.forEach(row => {
+          for (let i = 0; i < Math.abs(segmentsDifference); i++) {
+            row.lastChild?.remove();
+          }
+        });
+      }
+    };
+    window.addEventListener(textResize, handleResize);
+    return () => window.removeEventListener(textResize, handleResize);
+  }, []);
+
+  function getSkySegmentCount(): number {
+    const whiteSpaceAtSides: number = 8;
+    const breakpointXl: number = 1400;
+    const maxSkyLineWidth: number = Math.min(window.innerWidth, breakpointXl);
+    return Math.floor(maxSkyLineWidth/2 - whiteSpaceAtSides);
+  }
+
+  function getSkySegmentClass(): string {
+    const textSkyStar: string = 'sky-star';
+    const textTwinkle: string = 'twinkle';
     let segmentClass: string = '';
     if (!Math.floor(Math.random() * 60)) {
+      const isStarsAnimationVisible: boolean = !!document.querySelector(`.${textTwinkle}`);
       const starFadeFrequency: number = Math.floor(Math.random() * 5) + 1;
-      segmentClass = `${textSkyStar} ${textSkyStar}-${starFadeFrequency} `;
+      segmentClass = `${textSkyStar} ${textSkyStar}-${starFadeFrequency}${isStarsAnimationVisible ? ` ${textTwinkle}` : ''}`;
     }
+    return `${segmentClass} ${textMoonSpanSkySegment}`;
+  }
+
+  function renderSkySegment(skyLineIndex: number, skySegmentIndex: number): JSX.Element {
     return (
       <div
         key={`${textMoonSpanSkySegment}-${skyLineIndex}-${skySegmentIndex}`}
-        className={`${segmentClass}${textMoonSpanSkySegment}`}
+        className={getSkySegmentClass()}
       >
       </div>
     );
@@ -25,14 +62,14 @@ const MoonSpanSkyLines = React.memo(() => {
   function renderSkyLine(moonSpanSkyLine: string, index: number): JSX.Element {
     return (
       <div key={`${moonSpanSkyLine}-${index}`} className={moonSpanSkyLine}>
-        {Array(skySegmentsCount).fill(index).map(renderSkySegment)}
+        {Array(getSkySegmentCount()).fill(index).map(renderSkySegment)}
       </div>
     );
   }
 
   return (
-    <div className="moon-span-sky-lines">
-      {Array(76).fill('moon-span-sky-line').map(renderSkyLine)}
+    <div className={`${textMoonSpanSkyLine}s`}>
+      {Array(76).fill(textMoonSpanSkyLine).map(renderSkyLine)}
     </div>
   );
 });
