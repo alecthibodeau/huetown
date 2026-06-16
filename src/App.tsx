2189d00ebf29d3aea4d36ad54f279809687a5918
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2026-06-16 12:12:53 -0400

    feat: adjust date string to set boolean value of active shop variable

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ import formatText from './helpers/format-text';
 function App(): JSX.Element {
   const [isBreakpointXs, setIsBreakpointXs] = useState<boolean>(true);
   const [viewportWidth, setViewportWidth] = useState<number>(window.innerWidth);
-  const isShopActive: boolean = new Date() < new Date('2026-07-01');
+  const isShopActive: boolean = new Date() < new Date('2026-12-31');
 
   useEffect(() => {
     const breakpointSm: number = 576;
